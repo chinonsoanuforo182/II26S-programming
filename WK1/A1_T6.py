d7602ce9c32bec12c6147a06c6b484bd94edc08c
chinonsoanuforo182 <chinonsoanuforo182@gmail.com>
2026-09-21 11:26:03 +0300

    add WK1/A1_T6.py program

---
+++ WK1/A1_T6.py
@@ -0,0 +1,9 @@
+print("Insert an integer: ")
+Feed = input(479)
+
+Value = int(Feed)
+
+Remainder = Value % 2
+
+print(f"Value is {Value}")
+print(f"The remainder is {Remainder} when {Value} is divided by 2.")
