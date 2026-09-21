b73075427823c3b0b2e63f447c2a979876d6ec25
chinonsoanuforo182 <chinonsoanuforo182@gmail.com>
2026-09-21 11:18:37 +0300

    add WK1/A1_T5.py program

---
+++ WK1/A1_T5.py
@@ -0,0 +1,13 @@
+print("Calculate the area of a wall.")
+
+Feed = input("2")
+Width = int(Feed)
+
+Feed = input("5")
+Height = int(Feed)
+
+print(f"Width is {Width} m and height is {Height} m.")
+
+Area = Width * Height
+
+print(f"The wall will be {Area} square meters.")
